08bb91da84507ceb62ba5cdd2500cf0a1ca6a8cb
Hector Sanjuan <code@hector.link>
2020-04-21 23:41:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+mfs-replace-root
+================
+
+A small program to replace IPFS's MFS root.
+
+Build:
+
+```
+go build
+```
+
+Check current root:
+
+```
+./mfs-replace-root
+```
+
+Replace root:
+
+```
+./mfs-replace-root <new-root-cid>
+```
+
+(For demonstration purposes only).
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-datastore"
+	config "github.com/ipfs/go-ipfs-config"
+	"github.com/ipfs/go-ipfs/plugin/loader"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+)
+
+func setupPlugins(externalPluginsPath string) error {
+	// Load any external plugins if available on externalPluginsPath
+	plugins, err := loader.NewPluginLoader(filepath.Join(externalPluginsPath, "plugins"))
+	if err != nil {
+		return fmt.Errorf("error loading plugins: %s", err)
+	}
+
+	// Load preloaded and external plugins
+	if err := plugins.Initialize(); err != nil {
+		return fmt.Errorf("error initializing plugins: %s", err)
+	}
+
+	if err := plugins.Inject(); err != nil {
+		return fmt.Errorf("error initializing plugins: %s", err)
+	}
+
+	return nil
+}
+
+var rootKey = datastore.NewKey("/local/filesroot")
+
+func main() {
+	defaultPath, err := config.PathRoot()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := setupPlugins(defaultPath); err != nil {
+		log.Fatal(err)
+	}
+
+	repo, err := fsrepo.Open(defaultPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ds := repo.Datastore()
+	root, err := ds.Get(rootKey)
+	if err == datastore.ErrNotFound {
+		fmt.Println("empty MFS root")
+	} else if err != nil {
+		log.Fatal(err)
+	} else {
+		c, err := cid.Cast(root)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("MFS Root: %s\n", c)
+	}
+
+	if len(os.Args) < 2 {
+		log.Println("No new MFS root provided")
+		return
+	}
+
+	fmt.Println("Updating MFS root")
+	newRoot, err := cid.Parse(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ds.Put(rootKey, newRoot.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("MFS root updated to %s\n", newRoot)
+}
